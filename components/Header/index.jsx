3ed22f4849366681af934f557839a294bd43f960
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-11 15:17:30 +0100

    fichta e-commerce website abeg work

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -2,23 +2,17 @@ import React from 'react'
 
 // Components for Header
 import HeroSection from '../HeroSection/Index'
-import Navigation from '../Navigation/Index'
 
 // styles for Header
 import { Container } from '../../styles/Fichta_CSS'
 
-
 const Header = () => {
     return (
-        <div>
+        <>
             <Container bg="#F6F3FC">
-                <>
-                    <Navigation />
-                    <HeroSection />
-                </>
+                <HeroSection />
             </Container>
-
-        </div>
+        </>
     )
 }
 
