10faeb9d5922ad038252be5f81349e48c5564cc4
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-11 19:19:08 +0100

    fichta e-commerce website cart v2

--- components/Cart/CartComponent.jsx
+++ components/Cart/CartComponent.jsx
@@ -1,26 +1,76 @@
 import React from 'react'
+import Image from "next/image"
+import p1 from "../../public/logo/shirt.jpg"
 
 import {
   CartContainer, CartGrid,
-  CartGridCard, Button
+  CartGridCard, Button, Splitter,
+  CartInfomation, CartImageText,
+  CartPricing,
 } from "../../styles/Cart/CartMenu"
-import { Constraints } from "../../styles/Fichta_CSS"
+
+import { Constraints, Typography } from "../../styles/Fichta_CSS"
 
 const CartComponent = () => {
+  const [itemCount, setItemCount] = React.useState(0);
+
+  const IncreaseCountHandler = (count) => {
+    setItemCount(itemCount++)
+  }
+
+  const DecreaseCountHandler = () => {
+    if (itemCount < 1) {
+      return itemCount = 0;
+    } else {
+      setItemCount(itemCount--)
+    }
+  }
+
   return (
-    <>
-      <Constraints>
+    <Constraints>
+      <Splitter>
         <CartContainer>
+
           <CartGrid>
             <CartGridCard>
-              <h1>cartComponent</h1>
-              <h6>i am cart item dummas</h6>
+              <Typography>Cart (2) </Typography>
+              <br />
+              <hr />
+              <br />
+              <CartInfomation>
+                <CartImageText>
+                  <div className="img">
+                    <Image src={p1} alt="Image" width="100" height="100" />
+                  </div>
+                  <div className="text">
+                    <Typography>Playstation</Typography>
+                    <br />
+                    <p>TSES Rechargeable Standing Fan- HF-B22U Seller</p>
+                  </div>
+                </CartImageText>
+
+                <CartPricing>
+                  <Typography>$23.00</Typography>
+                  <div className="control">
+                    <button onClick={DecreaseCountHandler}>-</button>
+                    <h4>{itemCount}</h4>
+                    <button onClick={IncreaseCountHandler}>+</button>
+                  </div>
+                </CartPricing>
+              </CartInfomation>
+
             </CartGridCard>
           </CartGrid>
+
         </CartContainer>
 
-      </Constraints>
-    </>
+        <CartContainer>
+          <Typography>CART SUMMARY</Typography>
+          <br />
+          <hr />
+        </CartContainer>
+      </Splitter>
+    </Constraints>
   )
 }
 
--- styles/Cart/CartMenu.js
+++ styles/Cart/CartMenu.js
@@ -1,10 +1,23 @@
 import styled from "styled-components"
 
+export const Splitter = styled.div`
+width: 100%;
+ padding: 2em 1em;
+ margin: 1em 0;
+ display: grid;
+grid-template-columns: 2fr 1fr;
+gap: 1em;
+
+@media (max-width: 768px) {
+grid-template-rows: 1fr;
+grid-template-columns: 1fr;
+}
+`
+
 export const CartContainer = styled.div`
 width: 100%;
  box-shadow: 0 1px 5px 1px rgb(0 0 0 / 10%);
- padding: 2em 0;
- margin: 1em 0;
+ padding: 2em 1em;
 `
 
 export const CartGrid = styled.div`
@@ -16,9 +29,53 @@ justify-content: space-between;
 grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
 
 @media (max-width: 768px) {
-grid-template-rows: repeat(2, 1fr);
-grid-template-columns: repeat(2, 1fr);
+grid-template-rows: repeat(1, 1fr);
+grid-template-columns: repeat(1, 1fr);
 gap: 1em;
+}
+`
+
+export const CartInfomation = styled.div`
+width: 100%;
+display: grid;
+grid-template-columns: 2fr 1fr;
+align-items: center;
+gap: 2em;
+justify-content: space-between;
+
+@media (max-width: 768px) {
+grid-template-rows: 1fr;
+grid-template-columns: 1fr;
+}
+
+`
+
+export const CartImageText = styled.div`
+width: 100%;
+display: grid;
+grid-template-columns: 1fr 1fr;
+align-items: center;
+justify-content: space-between;
+gap: 2em;
+
+@media (max-width: 768px) {
+grid-template-rows: 1fr;
+grid-template-columns: 1fr;
+}
+
+`
+
+export const CartPricing = styled.div`
+width: 100%;
+display: grid;
+grid-template-columns: 1fr 1fr;
+align-items: center;
+justify-content: space-between;
+gap: 2em;
+
+@media (max-width: 768px) {
+grid-template-rows: 1fr;
+grid-template-columns: 1fr;
 }
 `
 
@@ -36,6 +93,25 @@ color: black;
 font-weight: bold;
 transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out, all 0.3s ease-in-out;
 text-align: start;
+
+.control{
+    display: flex;
+    gap: .5em;
+    align-items: center;
+
+button{
+padding: .5em .8em;
+border: none;
+border-radius: 3px;
+background-color: #C60A70;
+color: #ffffff;
+font-size: 1rem;
+font-weight: bold;
+cursor: pointer;
+box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+}
+
+}
 
    &:hover{
     //    background-color: #FFFCEB;
