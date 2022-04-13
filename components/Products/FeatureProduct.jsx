ec1b28d80ca1979c2c1b5a717dd18432963404fd
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-14 00:53:15 +0100

    fichta e-commerce cart summary completed v7

--- components/Products/FeatureProduct.jsx
+++ components/Products/FeatureProduct.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from "next/image"
+import Link from "next/link"
 
 import { BsBagPlus } from "react-icons/bs"
 import DummyProduct from "../../data/dummyProduct"
@@ -28,10 +29,18 @@ const Product = () => {
                             return (
                                 <Card key={product.id}>
                                     <div className="feature-img">
-                                        <Image src={product.image} alt={product.name} width="100%" height="100%" />
+                                        <Link href={`/shop/${product.id}`}>
+                                            <a style={{ color: "black", cursor: "pointer" }}>
+                                                <Image src={product.image} alt={product.name} width="100%" height="100%" />
+                                            </a>
+                                        </Link>
                                     </div>
                                     <div className="sp">
-                                        <Description>{product.name}</Description>
+                                        <Link href={`/shop/${product.id}`}>
+                                            <a style={{ color: "black" }}>
+                                                <Description>{product.name}</Description>
+                                            </a>
+                                        </Link>
                                         <br />
                                         <h6>{` ${product.desc} items`}</h6>
                                     </div>
