f31355a96ee8803971ec65635a7c9ac9a94787cb
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-13 19:28:37 +0100

    fichta e-commerce cart summary completed v2

--- components/Cart/CartComponent.jsx
+++ components/Cart/CartComponent.jsx
@@ -24,52 +24,61 @@ const CartComponent = () => {
     <Constraints>
       <Splitter>
         <CartContainer>
+          {
+            CartItems.length === 0 ? (
+              <>
+                <Constraints>
+                  <h1>
+                    Hey Dummy!! Your Cart is empty
+                  </h1>
+                </Constraints>
+              </>
+            ) : (
+              <>
+                <CartGridCard>
+                  <Typography>
+                    {` Cart (${CartItems.length})`}
+                  </Typography>
+                </CartGridCard>
+                {
+                  CartItems.map((cart) => {
+                    return (
+                      <div key={cart.id}>
+                        <CartGrid>
+                          <CartGridCard>
+                            <hr />
+                            <br />
+                            <CartInfomation>
+                              <CartImageText>
+                                <div className="img">
+                                  <Image src={cart.image} alt="Image" width="100" height="100" />
+                                </div>
+                                <div className="text">
+                                  <Typography>{cart.name}</Typography>
+                                  <br />
+                                  <Paragraph sizeM="12px" size="13px" style={{ fontSize: "12px", fontweight: "400" }}>{cart.description}</Paragraph>
+                                </div>
+                              </CartImageText>
 
-          <CartGrid>
-            <CartGridCard>
-              <Typography>
-                {` Cart (${CartItems.length})`}
-              </Typography>
+                              <CartPricing>
+                                <Typography color="#C5006F">{`$${cart.price}.00`}</Typography>
+                                <div className="control">
+                                  <button onClick={() => dispatch(DecreaseProductQuantity(cart.id))}>-</button>
+                                  <h4>{Quantity}</h4>
+                                  <button onClick={() => dispatch(IncreaseProductQuantity(cart.id))}>+</button>
+                                </div>
+                              </CartPricing>
+                            </CartInfomation>
 
-            </CartGridCard>
-          </CartGrid>
-
-          {CartItems.map((cart) => {
-            return (
-              <div key={cart.id}>
-                <CartGrid>
-                  <CartGridCard>
-
-                    <hr />
-                    <br />
-                    <CartInfomation>
-                      <CartImageText>
-                        <div className="img">
-                          <Image src={cart.image} alt="Image" width="100" height="100" />
-                        </div>
-                        <div className="text">
-                          <Typography>{cart.name}</Typography>
-                          <br />
-                          <Paragraph sizeM="12px" size="13px" style={{ fontSize: "12px", fontweight: "400" }}>{cart.description}</Paragraph>
-                        </div>
-                      </CartImageText>
-
-                      <CartPricing>
-                        <Typography color="#C5006F">{`$${cart.price}.00`}</Typography>
-                        <div className="control">
-                          <button onClick={() => dispatch(DecreaseProductQuantity(cart.id))}>-</button>
-                          <h4>{Quantity}</h4>
-                          <button onClick={() => dispatch(IncreaseProductQuantity(cart.id))}>+</button>
-                        </div>
-                      </CartPricing>
-                    </CartInfomation>
-
-                  </CartGridCard>
-                </CartGrid>
-              </div>
+                          </CartGridCard>
+                        </CartGrid>
+                      </div>
+                    )
+                  })
+                }
+              </>
             )
-          })}
-
+          }
         </CartContainer>
 
         <CartContainer>
