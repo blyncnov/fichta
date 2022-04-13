3669e298022be4b6d79f290b6d088fce0ffbb62c
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-13 20:05:51 +0100

    fichta e-commerce cart summary completed v4

--- pages/shop/[productid].js
+++ pages/shop/[productid].js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import {SingleProductContainer} from "../../styles/Products/SingleProduct"
+import {Constraints} from "../../styles/Fichta_CSS"
+
 const SingleProduct = () => {
   return (
       <>
-          <h1>SingleProduct</h1>
+      <Constraints>
+        <SingleProductContainer>
+            <h1>SingleProduct</h1>
+        </SingleProductContainer>
+        </Constraints>
       </>
   )
 }
---
+++ styles/Products/SingleProduct.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+
+export const SingleProductContainer = styled.div`
+width: 100%;
+position: relative;
+padding: 2em 0;
+box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 2px 0px;
+`
+
+export default SingleProductContainer  ;
