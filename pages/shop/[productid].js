7fef5c65017eb978d5c73d11c9d34a2f9e44572c
Taiwo Boluwatife <blyncnov@Taiwos-MacBook-Pro.local>
2022-04-14 00:47:52 +0100

    fichta e-commerce cart summary completed v6

--- pages/shop/[productid].js
+++ pages/shop/[productid].js
@@ -1,14 +1,59 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/router'
 
-import {SingleProductContainer} from "../../styles/Products/SingleProduct"
-import {Constraints} from "../../styles/Fichta_CSS"
+import dummyProduct from "../../data/dummyProduct"
+
+import IamgeCover from "../../public/logo/shirt.jpg"
+
+import { useDispatch } from "react-redux"
+import { AddToCart } from "../../redux/reducers/CartSlice"
+
+import {
+  SingleProductContainer, SingleProductDivider,
+  Button,
+} from "../../styles/Products/SingleProduct"
+import {Constraints, Description , Typography, HeadingOne ,Paragraph} from "../../styles/Fichta_CSS"
+
 
 const SingleProduct = () => {
+const dispatch = useDispatch()
+  const router = useRouter()
+
+const { productid } = router.query
+ console.log(productid);
+
+  const SingleProduct = dummyProduct.filter((product) => {
+    if (product.id === parseInt(productid) ) {
+      return product;
+    } else {
+      return "Item not Found";
+  }
+  })
+
+   console.log(SingleProduct);
+
   return (
       <>
       <Constraints>
         <SingleProductContainer>
-            <h1>SingleProduct</h1>
+          <SingleProductDivider>
+            <Image src={IamgeCover} alt="dummy" layout="responsive" />
+          </SingleProductDivider>
+           <SingleProductDivider>
+            <Description>Vegetables Category</Description> 
+            <br/>
+            <HeadingOne sizeM="1.3rem">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops </HeadingOne>
+            <br />
+            <Paragraph>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches)
+              in the padded sleeve, your everyday</Paragraph>
+            <br />
+            <HeadingOne size="1.5rem" sizeM="1.2rem">
+              $63.46
+            </HeadingOne>
+            <br/>
+            <Button onClick={dispatch(AddToCart(91))}>Add to Cart</Button>
+          </SingleProductDivider>
         </SingleProductContainer>
         </Constraints>
       </>
--- styles/Products/SingleProduct.js
+++ styles/Products/SingleProduct.js
@@ -5,7 +5,37 @@ export const SingleProductContainer = styled.div`
 width: 100%;
 position: relative;
 padding: 2em 0;
+display: grid;
+grid-template-columns: 1fr 1fr;
+gap: 1em;
+
+@media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-rows: 1fr;
+}
+`
+
+
+export const SingleProductDivider = styled.div`
+width: 100%;
+position: relative;
+padding: 2em 1em;
+background-color: transparent;
 box-shadow: rgba(0, 0, 0, 0.06) 0px 2px 2px 0px;
+`
+
+export const Button = styled.button`
+width: auto;
+height: auto;
+padding: 1em 2em;
+border: none;
+border-radius: 3px;
+background-color: #0168CD;
+color: #ffffff;
+font-size: 1rem;
+font-weight: bold;
+cursor: pointer;
+box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
 `
 
 export default SingleProductContainer  ;
